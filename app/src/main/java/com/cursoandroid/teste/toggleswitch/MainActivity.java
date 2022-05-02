44ee7814d93b3a080f1421277b7e2be77ad48017
Gabriel Rossi <costagabrielrd@gmail.com>
2022-05-02 16:19:34 -0300

    all listener

--- app/src/main/java/com/cursoandroid/teste/toggleswitch/MainActivity.java
+++ app/src/main/java/com/cursoandroid/teste/toggleswitch/MainActivity.java
@@ -38,12 +38,38 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
                 if ( isChecked ){
-                    textResult.setText("ligado");
+                    textResult.setText("Switch ligado");
                 }else{
-                    textResult.setText("desligado");
+                    textResult.setText("Switch desligado");
                 }
             }
         });
+
+        togglePassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if ( isChecked ){
+                    textResult.setText("Toggle ligado");
+                }else{
+                    textResult.setText("Toggle desligado");
+                }
+            }
+        });
+
+        checkPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if ( isChecked ){
+                    textResult.setText("Check ligado");
+                }else{
+                    textResult.setText("Check desligado");
+                }
+            }
+        });
+
+
 
     }
 
@@ -58,23 +84,22 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        /*if (togglePassword.isChecked()){
+        if (togglePassword.isChecked()){
             textResult.setText("Botão ligado");
         }else{
             textResult.setText("Botão desligado");
         }
 
-         */
 
 
 
-        /*if (checkPassword.isChecked()){
+
+        if (checkPassword.isChecked()){
             textResult.setText("Check marcado");
         }else {
             textResult.setText("Check desmarcado");
         }
-
-         */
+        
     }
 
 }
