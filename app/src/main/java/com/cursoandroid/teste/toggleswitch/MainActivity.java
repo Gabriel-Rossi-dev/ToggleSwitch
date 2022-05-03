0ffc8c0ad23082bca18b0d6b88c56b0430fcf31d
Gabriel Rossi <costagabrielrd@gmail.com>
2022-05-03 16:08:24 -0300

    listener automatico

--- app/src/main/java/com/cursoandroid/teste/toggleswitch/MainActivity.java
+++ app/src/main/java/com/cursoandroid/teste/toggleswitch/MainActivity.java
@@ -6,100 +6,135 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private Switch switchPassword;
     private ToggleButton togglePassword;
+    private Switch switchPassword;
     private TextView textResult;
-    private CheckBox checkPassword;
+    private RadioButton radioH, radioM, radioNb;
+    private CheckBox checkMail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        switchPassword = findViewById(R.id.switchPassword);
         togglePassword = findViewById(R.id.togglePassword);
+        switchPassword = findViewById(R.id.switchPassword);
         textResult = findViewById(R.id.textResult);
-        checkPassword = findViewById(R.id.checkPassword);
+        radioH = findViewById(R.id.radioH);
+        radioM = findViewById(R.id.radioM);
+        radioNb = findViewById(R.id.radioNb);
+        checkMail = findViewById(R.id.checkMail);
         addListener();
 
     }
 
+
     public void addListener(){
+
+        togglePassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    textResult.setText("Toggle ligado");
+                }else{
+                textResult.setText("Toggle desligado");
+            }
+            }
+        });
 
         switchPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-
-                if ( isChecked ){
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
                     textResult.setText("Switch ligado");
-                }else{
+                }else {
                     textResult.setText("Switch desligado");
                 }
             }
         });
 
-        togglePassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        radioH.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-
-                if ( isChecked ){
-                    textResult.setText("Toggle ligado");
-                }else{
-                    textResult.setText("Toggle desligado");
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    textResult.setText("Homem selecionado");
                 }
             }
         });
 
-        checkPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        radioM.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-
-                if ( isChecked ){
-                    textResult.setText("Check ligado");
-                }else{
-                    textResult.setText("Check desligado");
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    textResult.setText("Mulher selecionado");
                 }
             }
         });
 
+        radioNb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    textResult.setText("Não binário selecionado");
+                }
+            }
+        });
 
+        checkMail.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    textResult.setText("Receberá e-mails");
+                }else{
+                    textResult.setText("Não receberá e-mails");
+                }
+            }
+        });
 
     }
 
     public void send (View view){
 
-        if (switchPassword.isChecked()){
-            textResult.setText("Check marcado");
+        /*if(togglePassword.isChecked()){
+            textResult.setText("Toggle ligado");
         }else{
-            textResult.setText("Check desmarcado");
+            textResult.setText("Toggle desligado");
         }
 
+         */
 
-
-
-        if (togglePassword.isChecked()){
-            textResult.setText("Botão ligado");
-        }else{
-            textResult.setText("Botão desligado");
-        }
-
-
-
-
-
-        if (checkPassword.isChecked()){
-            textResult.setText("Check marcado");
+        /*if (switchPassword.isChecked()){
+            textResult.setText("Switch ligado");
         }else {
-            textResult.setText("Check desmarcado");
+            textResult.setText("Switch desligado");
         }
-        
+
+         */
+        if (radioH.isChecked()){
+            textResult.setText("Masculino Selecionado");
+        }else if (radioM.isChecked()){
+            textResult.setText("Feminino selecionado");
+        }else if (radioNb.isChecked()){
+            textResult.setText("Não binário selecionado");
+        }else {
+            textResult.setText("Selecione um gênero");
+        }
+
+        if(checkMail.isChecked()){
+            textResult.setText("Você receberá e-mails");
+        }else{
+            textResult.setText("Você não receberá e-mails");
+        }
     }
 
-}
+    }
+
